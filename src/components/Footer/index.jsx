ce827e9360222707a563b12b2e043de8ae17c333
Diogo Marques <diogo@fontainhas.com>
2019-12-06 11:39:13 +0000

    cenas

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Footer extends Component {
-  render() {
-    return (
-      <div>
-        <footer  bg="dark" variant="dark">
-  <a className="pt-3 pr-3 text-secondary text-right nav-link" href="/about">About Us © 2019</a>
-</footer>
-        
-      </div>
-    )
-  }
+	render() {
+		return (
+			<footer className="footer py-0 bg-dark">
+				<a
+					className="pt-3 pr-3 text-secondary nav-link border border-primary"
+					href="/about"
+				>
+					Link One
+				</a>
+				<a
+					className="pt-3 pr-3 text-secondary nav-link border border-primary"
+					href="/about"
+				>
+					Link Two
+				</a>
+				<a
+					className="pt-3 pr-3 text-secondary nav-link border border-primary"
+					href="/about"
+				>
+					Link Three
+				</a>
+			</footer>
+		);
+	}
 }
 
 export default Footer;
--- src/views/HeroView.jsx
+++ src/views/HeroView.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const HeroView = () => {
   return (
-    <div>
-      <Container fluid className="py-5">
+    <Jumbotron>
+      <Col fluid className="py-5">
         <Row>
           <Col md={1}></Col>
           <Col>
-            <h1 class="display-1">GAME NAME</h1>
+            <h1 class="display-1 font-weight-normal">GAME NAME</h1>
           </Col>
         </Row>
         <Row>
           <Col md={4}></Col>
           <Col md={5}>
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
           </Col>
         </Row>
-      </Container>
-    </div>
+      </Col>
+    </Jumbotron>
   )
 }
 

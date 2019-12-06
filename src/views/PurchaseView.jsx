ba54a06bbd17ed152af265a9214fdd0ccec10881
Diogo Marques <diogo@fontainhas.com>
2019-12-06 16:42:54 +0000

    changes

--- src/views/PurchaseView.jsx
+++ src/views/PurchaseView.jsx
@@ -3,23 +3,31 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 const PurchaseView = () => {
   return (
-    <Jumbotron id="purchase" fluid className="py-5 mb-0 PurchaseBackground">
-      <Row className="pt-5">
-        <Col md={1}></Col>
-        <Col>
-          <h1 className="display-4 font-archivo">PURCHASE</h1>
-        </Col>
-      </Row>
-      <Col>
-        <Col md={3} lg={2}></Col>
-        <Col md={5}>
-          <h1 className="font-weight-light pl-3">Bring The Good Place to your classroom</h1>
-          <Button variant="secondary">BUY THE GAME</Button>
-        </Col>
-      </Col>
+    <Jumbotron id="purchase" fluid className="pt-5 mb-0 pb-0 PurchaseBackground">
+      <Container className="d-flex justify-content-between align-items-center">
+        <div>
+          <Row className="pt-5">
+            <Col md={1}></Col>
+            <Col>
+            <h1 className="display-4 font-weight-bold text-left">PURCHASE</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={3} lg={2}></Col>
+            <Col>
+              <h1 className="font-weight-light pl-3">Bring The Good Place to your classroom</h1>
+              
+            </Col>
+          </Row>
+        </div>
+        <div>
+          <Button variant="secondary" className="btn-lg">BUY THE GAME</Button>
+        </div>
+      </Container>
     </Jumbotron>
   )
 }
